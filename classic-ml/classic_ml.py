3c846c88947668569edb154e98a88ec47abbb881
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-04-29 21:49:04 +0700

    Test classic ml to be the same as the paper

--- classic-ml/classic_ml.py
+++ classic-ml/classic_ml.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+import random
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
@@ -16,47 +16,97 @@ scaled_day_df = pd.read_csv("../processed/scaled_day.csv", index_col=0)
 
 scaled_1_hr_df = pd.read_csv("../processed/scaled_1_hr.csv", parse_dates=["Date Time"], index_col="Date Time")
 
-print(scaled_1_hr_df.head())
+# print(scaled_1_hr_df.head())
 
-input_hours = 168 # 24 * 7 for 7 days
-output_hours = 3
+# input_hours = 168 # 24 * 7 for 7 days
+# output_hours = 3
 
-X, y = [], []
-timestamps = scaled_1_hr_df.index
+# X, y = [], []
+# timestamps = scaled_1_hr_df.index
 
-for i in range(len(scaled_1_hr_df) - input_hours - output_hours):
-    # Extract input (past 7 days)
-    X.append(scaled_1_hr_df.iloc[i:i+input_hours].values)
+# for i in range(len(scaled_1_hr_df) - input_hours - output_hours):
+#     # Extract input (past 7 days)
+#     X.append(scaled_1_hr_df.iloc[i:i+input_hours].values)
 
-    # Extract output (next 3 hours)
-    y.append(scaled_1_hr_df.iloc[i+input_hours:i+input_hours+output_hours].values)
+#     # Extract output (next 3 hours)
+#     y.append(scaled_1_hr_df.iloc[i+input_hours:i+input_hours+output_hours].values)
 
-# Convert to NumPy arrays
-X, y = np.array(X), np.array(y)
+# # Convert to NumPy arrays
+# X, y = np.array(X), np.array(y)
 
-print("Input shape:", X.shape)  # Expected: (samples, 168, features)
-print("Output shape:", y.shape)  # Expected: (samples, 3, features)
+# print("Input shape:", X.shape)  # Expected: (samples, 168, features)
+# print("Output shape:", y.shape)  # Expected: (samples, 3, features)
 
 
 ############################## Random forest ##############################
-
-# Flatten time dimension for RandomForestRegressor
-X_flat = X.reshape(X.shape[0], -1)  # (samples, 168 * features)
-y_flat = y.reshape(y.shape[0], -1)  # (samples, 1 * features)
-
-# Split into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X_flat, y_flat, test_size=0.2, random_state=42)
+from sklearn.model_selection import KFold
+X = scaled_1_hr_df[:].values
+y = scaled_1_hr_df[:].values
 
 # Fit the model using all samples at once
-rf = RandomForestRegressor(n_estimators=100, random_state=94)
-rf.fit(X_train, y_train)
+num_trees = [100, 250, 500]
+for num_tree in num_trees:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random.randint(1,1000))
+    rf = RandomForestRegressor(n_estimators=num_tree, random_state=random.randint(1,1000))
+    kfold = KFold(n_splits=10)
+    # Using 10-fold cross validation to evaluate performance
+    mses_rf = []
+    for train_idx, val_idx in kfold.split(X_train, y_train):
+        X_train_fold, X_val_fold = X_train[train_idx], X_train[val_idx]
+        y_train_fold, y_val_fold = y_train[train_idx], y_train[val_idx]
+        rf.fit(X_train_fold, y_train_fold)
+        mse_rf = mean_squared_error(y_val_fold, rf.predict(X_val_fold))
+        mses_rf.append(mse_rf)
 
-# Predict next 3 hour
-y_pred = rf.predict(X_test)
+    mean_mse = np.mean(mses_rf)
+    std_mse = np.std(mses_rf)
+    # Evaluate performance with number of tree
+    print(f'MSE - RandomForest - {num_tree} trees: {mean_mse} +/- {std_mse}')
 
-# Evaluate performance
-mse_rf = mean_squared_error(y_test, y_pred)
-print(f'MSE - RandomForest: {mse_rf}')
 
-mse_rf_per_feature = mean_squared_error(y_test, y_pred, multioutput="raw_values")
-print("MSE per feature:", mse_rf_per_feature)  # Array of MSE values for each feature
+############################## SVR ##############################
+from sklearn.model_selection import GridSearchCV
+# Tuning C and gamma via grid search and find the best model
+svr = MultiOutputRegressor(SVR())
+
+C_range = [0.01, 0.1, 1, 10, 100, 1000, 10000]
+gamma_range = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.5, 0.6, 0.9]
+
+param_grid = [
+    {'estimator__C': C_range, 'estimator__kernel': ['linear']},
+    {'estimator__C': C_range, 'estimator__gamma': gamma_range, 'estimator__kernel': ['rbf']}
+]
+
+
+gs = GridSearchCV(estimator=svr, param_grid=param_grid, scoring='neg_mean_squared_error', cv=10, refit=True, n_jobs=-1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random.randint(1,1000))
+gs = gs.fit(X_train, y_train)
+print(f'MSE - SVR: {-gs.best_score_}')
+print(f'Best model: {gs.best_params_}')
+
+############################## LR ##############################
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random.randint(1,1000))
+lr = LinearRegression()
+kfold = KFold(n_splits=10)
+# Using 10-fold cross validation to evaluate performance
+mses_lr = []
+for train_idx, val_idx in kfold.split(X_train, y_train):
+    X_train_fold, X_val_fold = X_train[train_idx], X_train[val_idx]
+    y_train_fold, y_val_fold = y_train[train_idx], y_train[val_idx]
+    lr.fit(X_train_fold, y_train_fold)
+    mse_lr = mean_squared_error(y_val_fold, lr.predict(X_val_fold))
+    mses_lr.append(mse_lr)
+
+mean_mse = np.mean(mses_lr)
+std_mse = np.std(mses_lr)
+# Evaluate performance with number of tree
+print(mses_lr)
+print(f'MSE - LinearRegression: {mean_mse} +/- {std_mse}')
+
+# For whatever fucking reason LinearRegression outperform others ?????
+# MSE - RandomForest - 100 trees: 0.07447493090282847 +/- 0.0678771238265784
+# MSE - RandomForest - 250 trees: 0.07241612282924667 +/- 0.07542657798448549
+# MSE - RandomForest - 500 trees: 0.04760942694992552 +/- 0.004098491748925844
+# MSE - SVR: 0.00282000991060531
+# Best model: {'estimator__C': 1, 'estimator__kernel': 'linear'}
+# MSE - LinearRegression: 2.5782636268150114e-30 +/- 2.5832615415962112e-30
