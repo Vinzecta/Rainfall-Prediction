9613a6a246fe6434ab795f436d93686a5d168790
Lmao1232j <vinhtran23042004@gmail.com>
2025-05-04 15:56:29 +0700

    Finish preprocessing (6h interval data)

--- Data Analyst/preprocessing.py
+++ Data Analyst/preprocessing.py
@@ -27,24 +27,10 @@ if (checker_2):
 else:
     print("There is no case")
 
-data['Is Rain'] = np.where((data['rain (mm)'] > 0) & (data['raining (s)'] > 0), 1, 0)
-data.head()
-
-
-#Resample data
-def round_mean(value):
-    if 0 < value < 1:
-        return 1
-    return value
-
 resample_data = data.resample('6h').mean()
-resample_data['Is Rain'] = resample_data['Is Rain'].apply(round_mean)
-
 
 #Preprocessing data
 scaler = StandardScaler()
-new_data = resample_data.drop(columns=['Is Rain'])
-scaled_data = scaler.fit_transform(new_data)
-scaled_data = pd.DataFrame(scaled_data, columns=new_data.columns, index=new_data.index)
-scaled_data['Is Rain'] = resample_data['Is Rain']
+scaled_data = scaler.fit_transform(resample_data)
+scaled_data = pd.DataFrame(scaled_data, columns = resample_data.columns, index = resample_data.index)
 scaled_data.head()
